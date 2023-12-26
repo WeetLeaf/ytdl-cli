8a88f9416072d0e1729693d7ae7babdbcba9eb6d
MartinPELCAT <martinpelcat@hotmail.com>
2023-12-26 23:45:03 +0100

    Cleanup

--- src/services/promise.service.ts
+++
@@ -1,9 +0,0 @@
-
-export const countSuccess = <T>(
-  promises: PromiseSettledResult<T>[]
-): number => {
-  return promises.reduce((acc, promise) => {
-    if (promise.status === "fulfilled") return acc + 1;
-    return acc;
-  }, 0);
-};
